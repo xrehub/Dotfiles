415194c5887b7777877dfbcf854e772ba40a3c5a
xrehub <hubert.kielbasinski99@wp.pl>
2025-11-10 21:32:34 +0100

    add install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+# Simple dotfiles installer
+
+echo "Installing dotfiles.."
+
+# copying files to your home directory
+cp bash/.bashrc ~/.bashrc
+cp vim/.vimrc ~/.vimrc
+
+echo "Dotfiles installed!"
+echo "To apply changes, run: source ~/.bashrc"
